03d4353bafc412863f1ae8178193278f230a19f3
Drew Cook <drewcook29@gmail.com>
2021-03-27 12:52:37 -0600

    implement Material UI and sass

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,37 @@
-import '../styles/globals.css'
+import { useEffect } from 'react';
+import { AppBar, Container, CssBaseline } from '@material-ui/core';
+import { ThemeProvider } from '@material-ui/core/styles';
+import theme from '../styles/theme';
+import '../styles/globals.scss'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+const BeerBuddy = (props) => {
+	const { Component, pageProps } = props;
 
-export default MyApp
+	useEffect(() => {
+		// Remove the server-side injected CSS to fix MUI className mismatch errors
+		const jssStyles = document.querySelector('#jss-server-side');
+		if (jssStyles) {
+			jssStyles.parentElement.removeChild(jssStyles);
+		}
+	}, []);
+
+  return (
+		<>
+			<ThemeProvider theme={theme}>
+				<CssBaseline />
+				{/* navigation */}
+				<AppBar position="static">
+					<Container>
+						app bar here
+					</Container>
+				</AppBar>
+				{/* content */}
+				<Container>
+					<Component {...pageProps} />
+				</Container>
+			</ThemeProvider>
+		</>
+	);
+};
+
+export default BeerBuddy;
