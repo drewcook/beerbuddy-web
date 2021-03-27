8ddda751fa61dc43e095e5c553be0269ee42702b
Drew Cook <drewcook29@gmail.com>
2021-03-26 19:05:15 -0600

    update homepage and footer

--- pages/index.js
+++ pages/index.js
@@ -5,29 +5,27 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>BeerBuddy - Home</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          BeerBuddy
+          Welcome to BeerBuddy!
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          We keep track of your drinking habits so you don't have to!  Cheers!
         </p>
       </main>
 
       <footer className={styles.footer}>
         <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          href="https://altheawebservices.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+          Built by Althea Web Services
         </a>
       </footer>
     </div>
