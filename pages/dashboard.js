abe325846d10460a2ac900920210baf0e8b2402a
Drew Cook <drewcook29@gmail.com>
2021-03-28 17:37:51 -0600

    dashboard rough outline

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
-import { Typography } from '@material-ui/core'
+import { Grid, List, ListItem, Paper, Typography } from '@material-ui/core'
 import baseStyles from '../styles/base.module.scss'
+import styles from '../styles/dashboard.module.scss'
 
 const DashboardPage = props => (
 	<>
@@ -12,18 +13,39 @@ const DashboardPage = props => (
 		<Typography variant="h3" className={baseStyles.pageTitle}>
 			My Dashboard
 		</Typography>
-		<ul>
-			<li>Display # of lists</li>
-			<li>Display recent history events</li>
-			<li>Display favorites</li>
-			<li>
-				Display statistics
-				<ul>
-					<li># beers logged</li>
-					<li># breweries checked into</li>
-				</ul>
-			</li>
-		</ul>
+
+		<Grid container spacing={3}>
+			<Grid item md={6}>
+				<Paper className={styles.paper}>
+					<Typography variant="h4">My Lists</Typography>
+					<List>
+						<ListItem>Total: x</ListItem>
+						<ListItem>Recentl Updated: x</ListItem>
+					</List>
+				</Paper>
+				<Paper className={styles.paper}>
+					<Typography variant="h4">My Favorites</Typography>
+					<List>
+						<ListItem>Odell Imperial IPA</ListItem>
+						<ListItem>Pelican Brewing Company</ListItem>
+						<ListItem>New Image Brewery</ListItem>
+						<ListItem>My Super Sour List</ListItem>
+					</List>
+				</Paper>
+			</Grid>
+			<Grid item md={6}>
+				<Paper className={styles.paper}>
+					<Typography variant="h4">My Stats</Typography>
+					<List>
+						<ListItem>Beers Logged: x</ListItem>
+						<ListItem>Brewery Check Ins: x</ListItem>
+					</List>
+				</Paper>
+				<Paper className={styles.paper}>
+					<Typography variant="h4">Recent History</Typography>
+				</Paper>
+			</Grid>
+		</Grid>
 	</>
 )
 
