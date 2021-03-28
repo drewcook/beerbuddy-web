b452ebc96ac8cf2d92aed2250988a9165db546a3
Drew Cook <drewcook29@gmail.com>
2021-03-28 01:59:38 -0600

    dashboard placeholder

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,11 +1,30 @@
-import React from 'react'
-import { Button } from '@material-ui/core'
+import Head from 'next/head'
 
-const DashboardPage = WrappedComponent => props => {
-	const handleHello = msg => {
-		console.log(msg)
-	}
-	return <div onClick={() => handleHello('woah')}>hello world</div>
+const DashboardPage = props => {
+	return (
+		<>
+			<Head>
+				<title>BeerBuddy - My Dashboard</title>
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+
+			<div>
+				<h2>My Dashboard</h2>
+				<ul>
+					<li>Display # of lists</li>
+					<li>Display recent history events</li>
+					<li>Display favorites</li>
+					<li>
+						Display statistics
+						<ul>
+							<li># beers logged</li>
+							<li># breweries checked into</li>
+						</ul>
+					</li>
+				</ul>
+			</div>
+		</>
+	)
 }
 
 export default DashboardPage
