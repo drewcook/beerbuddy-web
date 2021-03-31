98255940aa2aae6ea27a69ab2892ae1172daae15
Drew Cook <drewcook29@gmail.com>
2021-03-30 22:35:53 -0600

    helpful comment

--- pages/api/graphql.js
+++ pages/api/graphql.js
@@ -1,5 +1,7 @@
 import apolloServer from '../../lib/apollo-server/apolloServer'
 
+// This prevents Next from parsing the body of incoming HTTP requests
+// and to prefer our GraphQL server instead
 export const config = {
 	api: {
 		bodyParser: false,
