ba04bea5b3f6e24feb820bdf1c65fdfe8f9cdfa1
Drew Cook <drewcook29@gmail.com>
2021-04-01 17:01:21 -0600

    update user lists page query

--- pages/user/lists.js
+++ pages/user/lists.js
@@ -1,9 +1,10 @@
 import { Button, Card, CardActions, CardContent, Typography } from '@material-ui/core'
-import { initializeApollo, addApolloState } from '../../lib/apollo-client/apolloClient'
-import { gql } from '@apollo/client'
+import { useQuery, gql } from '@apollo/client'
+import LoadingState from '../../components/LoadingState'
+import { useViewer } from '../../components/ViewerContext'
 
 const USER_LISTS_QUERY = gql`
-	query GetUserLists($userId: String!) {
+	query GetUserLists($userId: ID!) {
 		userLists(userId: $userId) {
 			_id
 			name
@@ -11,17 +12,25 @@ const USER_LISTS_QUERY = gql`
 	}
 `
 
-const UserListsPage = ({ lists }) => {
+const UserListsPage = () => {
+	const { viewer } = useViewer()
+	const { loading, data, error } = useQuery(USER_LISTS_QUERY, {
+		variables: { userId: viewer._id },
+	})
+
+	if (loading) return <LoadingState />
+	if (error) return <Typography color="error">Error occurred</Typography>
+
 	return (
 		<div>
 			<Typography variant="h3">My Lists</Typography>
-			{!lists.length ? (
+			{!data.userLists.length ? (
 				<Typography color="error">No Lists Found</Typography>
 			) : (
-				lists.map(list => (
-					<Card>
+				data.userLists.map(list => (
+					<Card key={list._id}>
 						<CardContent>
-							<Typography>ID: {list.id}</Typography>
+							<Typography>ID: {list._id}</Typography>
 							<Typography>Name: {list.name}</Typography>
 						</CardContent>
 						<CardActions>
@@ -34,21 +43,6 @@ const UserListsPage = ({ lists }) => {
 			)}
 		</div>
 	)
-}
-
-export const getServerSideProps = async ctx => {
-	const apolloClient = initializeApollo(ctx)
-
-	const { data } = await apolloClient.query({
-		query: USER_LISTS_QUERY,
-		variables: { userId: '12345 ' },
-	})
-
-	console.log({ data }, data.userLists)
-
-	return addApolloState(apolloClient, {
-		props: { lists: data.userLists },
-	})
 }
 
 export default UserListsPage
