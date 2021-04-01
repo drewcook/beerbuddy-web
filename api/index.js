822e5e283d64fabe0abd6ad10850c8735e3bf081
Drew Cook <drewcook29@gmail.com>
2021-04-01 17:00:43 -0600

    add back in axios interceptor for auth

--- api/index.js
+++ api/index.js
@@ -1,7 +1,24 @@
+import axios from 'axios'
 import * as auth from './auth'
 import * as breweryDb from './breweryDb'
 import * as lists from './lists'
 import * as users from './users'
+
+// Setup auth token for authorization header on all axios calls
+axios.interceptors.request.use(
+	request => {
+		// Exclude adding this for BreweryDB endpoints
+		// TODO: not all routes need this header..
+		if (!request.url.includes('brewerydb.com')) {
+			if (sessionStorage.getItem('auth-token')) {
+				request.headers['x-auth-token'] = sessionStorage.getItem('auth-token')
+			}
+		}
+
+		return request
+	},
+	error => error,
+)
 
 export const authService = auth
 export const breweryDbService = breweryDb
