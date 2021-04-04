846b0ca8c50d678adfe880125e56a2dd45b2fe8b
Drew Cook <drewcook29@gmail.com>
2021-04-04 09:54:44 -0600

    prettier imports

--- prettier.config.js
+++ prettier.config.js
@@ -1,4 +1,7 @@
 module.exports = {
 	...require('altheajs-prettier-config'),
 	semi: false,
+	importOrder: ['^[./]'], //['^@core/(.*)$', '^@server/(.*)$', '^@ui/(.*)$', '^[./]'],
+	importOrderSeparation: true,
+	experimentalBabelParserPluginsList: ['jsx', 'typescript'], // not using typescript ATM but might, so prepping
 }
