529b040729910160cbf560b77435f99814aca4ce
Drew Cook <drewcook29@gmail.com>
2021-04-07 22:01:25 -0600

    add in google analytics tag

--- pages/_document.js
+++ pages/_document.js
@@ -14,6 +14,18 @@ export default class MyDocument extends Document {
 						rel="stylesheet"
 						href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
 					/>
+					{/* Google Analytics */}
+					<script async src="https://www.googletagmanager.com/gtag/js?id=G-6RWQTDLEET"></script>
+					<script
+						dangerouslySetInnerHTML={{
+							__html: `
+								window.dataLayer = window.dataLayer || [];
+								function gtag(){dataLayer.push(arguments);}
+								gtag('js', new Date());
+								gtag('config', 'G-6RWQTDLEET');
+							`,
+						}}
+					/>
 				</Head>
 				<body>
 					<Main />
