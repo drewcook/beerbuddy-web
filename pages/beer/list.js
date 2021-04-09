b080d82595895782fa0725cb9134b51e56ebc29c
Drew Cook <drewcook29@gmail.com>
2021-04-09 17:30:36 -0600

    cleaner beer list

--- pages/beer/list.js
+++ pages/beer/list.js
@@ -1,8 +1,8 @@
-import { useQuery } from '@apollo/client'
+import { useLazyQuery } from '@apollo/client'
 import { Grid, Typography } from '@material-ui/core'
 import _get from 'lodash/get'
 import Head from 'next/head'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { BEER_LIST_QUERY } from '@bb/lib/apollo-client/shemas'
 import BeerCard from '@bb/components/BeerCard'
 import ListPagination from '@bb/components/ListPagination'
@@ -13,61 +13,69 @@ import PageTitle from '@bb/components/PageTitle'
 const BUGGY_BEER_IDS = ['p1tFbP']
 
 const BeerListPage = () => {
-	const { data, loading, error } = useQuery(BEER_LIST_QUERY, { variables: { page: 1 } })
-	const results = _get(data, 'beerList')
+	const [page, setPage] = useState(1)
+	const [getBeer, { data, loading, error }] = useLazyQuery(BEER_LIST_QUERY, { variables: { page } })
+
+	const results = _get(data, 'beerList.data', [])
 	const pageInfo = {
-		currentPage: _get(results, 'currentPage', 0),
-		numberOfPages: _get(results, 'numberOfPages', 0),
-		totalResults: _get(results, 'totalResults', 0),
+		currentPage: _get(data, 'beerList.currentPage', 0),
+		numberOfPages: _get(data, 'beerList.numberOfPages', 0),
+		totalResults: _get(data, 'beerList.totalResults', 0),
 	}
 
 	const handlePrevPage = async () => {
-		// 	if (pageInfo.page === 1) return
-		// 	if (error) setError(null)
-		// 	setLoading(true)
-		// 	try {
-		// 		const resp = await breweryDbService.getBeers(pageInfo.page - 1)
-		// 		const { data, currentPage, numberOfPages, totalResults } = resp
-		// 		setResults(data)
-		// 		setPageInfo({
-		// 			page: currentPage,
-		// 			totalPages: numberOfPages,
-		// 			totalResults,
-		// 		})
-		// 		setLoading(false)
-		// 	} catch (ex) {
-		// 		setError('An error occurred while getting results.')
-		// 		setLoading(false)
-		// 		console.error(ex)
-		// 	}
+		if (page === 1) return
+		setPage(page - 1)
 	}
 
 	const handleNextPage = async () => {
-		// 	if (pageInfo.page === pageInfo.numberOfPages) return
-		// 	if (error) setError(null)
-		// 	setLoading(true)
-		// 	try {
-		// 		const resp = await breweryDbService.getBeers(pageInfo.page + 1)
-		// 		const { data, currentPage, numberOfPages, totalResults } = resp
-		// 		setResults(data)
-		// 		setPageInfo({
-		// 			page: currentPage,
-		// 			totalPages: numberOfPages,
-		// 			totalResults,
-		// 		})
-		// 		setLoading(false)
-		// 	} catch (ex) {
-		// 		setError('An error occurred while getting results.')
-		// 		setLoading(false)
-		// 		console.error(ex)
-		// 	}
+		if (page === pageInfo.numberOfPages) return
+		setPage(page + 1)
 	}
 
-	if (loading) return <LoadingState />
-	if (error)
+	useEffect(async () => {
+		try {
+			await getBeer()
+		} catch (ex) {
+			console.error(ex)
+		}
+	}, [])
+
+	useEffect(async () => {
+		try {
+			await getBeer()
+		} catch (ex) {
+			console.error(ex)
+		}
+	}, [page])
+
+	const renderContent = () => {
+		if (loading) return <LoadingState />
+
+		if (error)
+			return (
+				<Typography color="error">Sorry, an error occurred while getting the results.</Typography>
+			)
+
 		return (
-			<Typography color="error">Sorry, an error occurred while getting the results.</Typography>
+			<>
+				<ListPagination
+					pageInfo={pageInfo}
+					onPrevPage={handlePrevPage}
+					onNextPage={handleNextPage}
+				/>
+				<Grid container spacing={3}>
+					{results
+						.filter(b => !BUGGY_BEER_IDS.includes(b.id))
+						.map(beer => (
+							<Grid item xs={12} sm={6} md={4} key={beer.id}>
+								<BeerCard beer={beer} />
+							</Grid>
+						))}
+				</Grid>
+			</>
 		)
+	}
 
 	return (
 		<>
@@ -77,18 +85,7 @@ const BeerListPage = () => {
 			</Head>
 
 			<PageTitle title="Beer List" headline="Search Beer" />
-
-			<ListPagination pageInfo={pageInfo} onPrevPage={handlePrevPage} onNextPage={handleNextPage} />
-
-			<Grid container spacing={3}>
-				{results.data
-					.filter(b => !BUGGY_BEER_IDS.includes(b.id))
-					.map(beer => (
-						<Grid item xs={12} sm={6} md={4} key={beer.id}>
-							<BeerCard beer={beer} />
-						</Grid>
-					))}
-			</Grid>
+			{renderContent()}
 		</>
 	)
 }
