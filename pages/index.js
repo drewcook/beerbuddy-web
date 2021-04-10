f94f111dfb669198365cb13044174e410f597716
Drew Cook <drewcook29@gmail.com>
2021-04-09 20:02:03 -0600

    homepage text

--- pages/index.js
+++ pages/index.js
@@ -30,7 +30,13 @@ const WelcomePage = () => {
 				</Head>
 				<PageTitle title="" headline="Welcome to BeerBuddy!" />
 				<Typography variant="h6">
-					We keep track of your drinking habits so you don't have to! Cheers!
+					We keep track of your drinking habits so you don't have to!
+				</Typography>
+				<Typography>
+					You can search through our data of beer and breweries and create lists for yourself to
+					keep a journal of your drinking journey. There are so many beers out there, so why not
+					create a list of one you'd like to try? Or how about breweries you'd like to visit? And
+					we've all had a beer or two we wouldn't try again. Sky's the limit for how you like roll!
 				</Typography>
 				<Card className={[styles.homeCentered, styles.card].join(' ')}>
 					<Typography variant="h5" gutterBottom>
